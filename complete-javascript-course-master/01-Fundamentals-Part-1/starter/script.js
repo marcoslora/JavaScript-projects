53c7f0650985a011dd18b3b07662b0c73ea85d7f
marcoslora <marcoslora95@gmail.com>
2022-09-16 08:56:19 -0400

    Create script.js

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,59 @@
+let js = "Amazing";
+// if (js === "Amazing") alert("Marcos");
+/* 
+comentario multilinea
+*/
+console.log(49 + 50);
+let first_name = "Bob";
+let lastName = "Perez"
+console.log(first_name);
+let myCountry = "Dominican Republic";
+let myContinent = "Continent Americano";
+let myPopulation = "11 millon of people";
+console.log(myContinent);
+console.log(myCountry);
+console.log(myPopulation);
+let javaScript = true;
+console.log(javaScript);
+// Ver el tipo de dato
+console.log(typeof true);
+// Reasignar variable
+first_name = "Pablo";
+console.log(first_name);
+// undefined variable
+let year;
+console.log(year);
+year = 2022;
+console.log(year);
+// null is a undefined but in javascript have a error an define like a object
+console.log(typeof null);
+// const son variables constantes y let puede ser cambiada; solo se utiliza const; no uses var old 
+let userAge = 30;
+const birthYear = 1992;
+var userName = "Marcos";
+// Operators
+const ageMarcos = year - birthYear;
+console.log(first_name ,ageMarcos);
+console.log(20 % 2);
+console.log(first_name + " " + lastName); 
+let x = 10 + 5;
+x += 10;
+x *= 10;
+x++;// x + 1
+x--;// x - 1
+console.log(x);
+
+// comparison operators
+console.log(10 > 11);// >, <, >=, <=
+console.log(12 >= 11)
+console.log(10 - 5 > 20 - 18)
+
+const markWeight = 78
+const markTall = 1.69
+const calcMarkBMI = markWeight/(markTall**2)
+const johnWeight = 92
+const johnTall = 1.95
+const johnMarkBMI = markWeight/(markTall**2)
+const comparisonBmi = calcMarkBMI > johnMarkBMI
+console.log(comparisonBmi)
+
