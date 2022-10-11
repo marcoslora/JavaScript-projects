00bd12e6096dc3424b003c304676667a6f90efe7
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-10-11 18:50:35 -0400

    Avances del crud de placas

--- Fabian projects/crud-placas/db.js
+++ Fabian projects/crud-placas/db.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.0/firebase-app.js";
-import { } from "https://www.gstatic.com/firebasejs/9.12.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, onSnapshot, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.12.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,11 @@ appId: "1:598648350091:web:b4fe5f709f5755cf553b8e"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+export const saveTask = (title, description) => {
+    addDoc(collection(db, 'tasks'),{title: title, descripcion: description});
+};
+export const getTasks = () => getDocs(collection(db, 'tasks'));
+export const onGetTasks = (callback) => onSnapshot(collection(db,'tasks'), callback);
 
-const saveTask = (title, description) => {
-    console.log(title, description)
-}
+export const deleteTasks = id => deleteDoc(doc(db, 'tasks', id));
--- Fabian projects/crud-placas/main.js
+++ Fabian projects/crud-placas/main.js
@@ -1,11 +1,42 @@
-window.addEventListener("DOMContentLoaded", () => {
-    console.log("funca")
-})
+import {saveTask, getTasks, onGetTasks, deleteTasks} from './db.js'
+
+const taskscontainer = document.getElementById('tasks-container')
+// querysnapshot los datos que existen en ese momento como es asincrono tiene q esperar los datos con un promesa
+window.addEventListener('DOMContentLoaded', async () => {
+    
+    onGetTasks ((querySnapshot) => {
+        let html = ''
+        querySnapshot.forEach(docs =>{
+            const tasks = docs.data();
+            html += `
+            <div>
+                <h3>${tasks.title}</h3>
+                <p>${tasks.descripcion}</p>
+                <button class='btn-delete' data-id="${docs.id}">Delete</button>
+                <button class='btn-edit' data-id="${docs.id}">Edit</button>
+            </div>`;
+        });
+        taskscontainer.innerHTML = html;
+        const btnsDelete = taskscontainer.querySelectorAll('.btn-delete')
+        btnsDelete.forEach(btn => {
+            btn.addEventListener('click', ({target: {dataset}}) => {
+                deleteTasks(dataset.id);
+            });
+        });
+        const btnEdit = taskscontainer.querySelectorAll('.btn-edit')
+        btnEdit.forEach(btn => {
+            btn.addEventListener('click', e =>{
+                console.log(e.target.dataset.id);
+            });
+        });
+    });
+});
 
 const taskForm = document.getElementById('task-form')
-taskForm.addEventListener("submit", (e) => {
+taskForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const title = taskForm['task-title'].value
-    const descripcion = taskForm['task-description'].value
-    console.log(title, descripcion)
+    const title = taskForm['task-title'];
+    const descripcion = taskForm['task-description'];
+    saveTask(title.value, descripcion.value);
+    taskForm.reset();  
 })
