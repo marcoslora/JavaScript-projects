25163834a97d8190a7cc56de957b2c920b09c26b
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-10-12 15:31:26 -0400

    Cambios en la actualizacion de los datos de la base de datos

--- Fabian projects/crud-placas/main.js
+++ Fabian projects/crud-placas/main.js
@@ -1,56 +1,63 @@
-import {saveTask, getTasks, onGetTasks, deleteTasks, getTask} from './db.js'
+import {
+  saveTask,
+  getTasks,
+  onGetTasks,
+  deleteTasks,
+  getTask,
+  updateTask,
+} from './db.js';
 
-const taskscontainer = document.getElementById('tasks-container')
+const taskscontainer = document.getElementById('tasks-container');
 // editar los datos
 let editStatus = false;
 
 // querysnapshot los datos que existen en ese momento como es asincrono tiene q esperar los datos con un promesa
 window.addEventListener('DOMContentLoaded', async () => {
-    //Loop de datos en la pantalla
-    onGetTasks ((querySnapshot) => {
-        let html = ''
-        querySnapshot.forEach(docs =>{
-            const tasks = docs.data();
-            html += `
+  //Loop de datos en la pantalla
+  onGetTasks(querySnapshot => {
+    let html = '';
+    querySnapshot.forEach(docs => {
+      const tasks = docs.data();
+      html += `
             <div>
                 <h3>${tasks.title}</h3>
                 <p>${tasks.description}</p>
                 <button class='btn-delete' data-id="${docs.id}">Delete</button>
                 <button class='btn-edit' data-id="${docs.id}">Edit</button>
             </div>`;
-        });
-        taskscontainer.innerHTML = html;
-        //Boton de eliminar
-        const btnsDelete = taskscontainer.querySelectorAll('.btn-delete')
-        btnsDelete.forEach(btn => {
-            btn.addEventListener('click', ({target: {dataset}}) => {
-                deleteTasks(dataset.id);
-            });
-        });
-        //Boton de editar
-        const btnEdit = taskscontainer.querySelectorAll('.btn-edit')
-        btnEdit.forEach(btn => {
-            btn.addEventListener('click', async (e) =>{
-                const doc = await getTask(e.target.dataset.id);
-                const task = doc.data();
-                taskForm['task-title'].value = task.title;
-                taskForm['task-description'].value = task.description;
-                editStatus = true;
-            });
-        });
     });
+    taskscontainer.innerHTML = html;
+    //Boton de eliminar
+    const btnsDelete = taskscontainer.querySelectorAll('.btn-delete');
+    btnsDelete.forEach(btn => {
+      btn.addEventListener('click', ({ target: { dataset } }) => {
+        deleteTasks(dataset.id);
+      });
+    });
+    //Boton de editar
+    const btnEdit = taskscontainer.querySelectorAll('.btn-edit');
+    btnEdit.forEach(btn => {
+      btn.addEventListener('click', async e => {
+        const doc = await getTask(e.target.dataset.id);
+        const task = doc.data();
+        taskForm['task-title'].value = task.title;
+        taskForm['task-description'].value = task.description;
+        editStatus = true;
+      });
+    });
+  });
 });
 //Guardar formulario
-const taskForm = document.getElementById('task-form')
-taskForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const title = taskForm['task-title'];
-    const descripcion = taskForm['task-description'];
-    //Condicional para guardar lo editado
-    if (editStatus) {
-        console.log('updating');
-    } else{
-        saveTask(title.value, descripcion.value);
-    };
-    taskForm.reset();  
-})
+const taskForm = document.getElementById('task-form');
+taskForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const title = taskForm['task-title'];
+  const descripcion = taskForm['task-description'];
+  //Condicional para guardar lo editado o actulizar lo editado
+  if (editStatus) {
+    updateTask(title.value, descripcion.value);
+  } else {
+    saveTask(title.value, descripcion.value);
+  }
+  taskForm.reset();
+});
