c94672a4ffeafc81314c84884c41c86f85ad9225
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-10-14 18:55:03 -0400

    manipulacon del DOM

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,4 +1,10 @@
 'use strict';
+// DOM document object model una representacion estructurada de los documentos de HTML
 // Selecionar elemento y su texto
 // clases . y id # como en css
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value;
