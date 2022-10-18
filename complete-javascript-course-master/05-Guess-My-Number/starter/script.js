6923c28843f7aa2d6149c57a95f6938ff4ed8404
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-10-17 22:39:32 -0400

    Guess the number

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -2,9 +2,28 @@
 // DOM document object model una representacion estructurada de los documentos de HTML
 // Selecionar elemento y su texto
 // clases . y id # como en css
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number';
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value;
+document.querySelector('.guess').value = 23;
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!!';
+  }
+});
