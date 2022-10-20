5251482f12fcc7151a21a011dc9ff63105df491e
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-10-20 15:44:40 -0400

    Update

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -3,4 +3,18 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-for (let i = 0; i < btnOpenModal.length; i++) console.log(btnOpenModal[i]);
+const openModal = function () {
+  // No se utiliza punto para las clases, se pueden agregar varia clases paradas por coma.
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
