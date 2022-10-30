1f43b8a72fa3b0c260267b8f3cd6bdeadab089ab
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-10-30 15:38:55 -0400

    Definitions JS

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+//high-level languages setup the memory que vamos a utilizar automaticamente
+//Garbage-collected siempre limpia la memoria de la pc
+//Interpreted or just-in-time compiled
+//Multi-paradigm; procedural programming, object-oriented programming(opp), functional programming(fp)
+//Prototype-based object-oriented tiene una base de OPP en todo lo q se crea
+//Firts-class functions las funciones se tratan como variables
+//Dynamic data type the engine of JS changed  automatically.
+//Single-threaded solo hace una cosa a la vez
+//Non-blocking event loop
