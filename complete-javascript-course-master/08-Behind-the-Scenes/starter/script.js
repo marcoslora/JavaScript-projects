72a3ccf3bc183fe736f78847b14943749643ad6d
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-10-30 22:12:31 -0400

    Avances

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -8,3 +8,5 @@
 //Dynamic data type the engine of JS changed  automatically.
 //Single-threaded solo hace una cosa a la vez
 //Non-blocking event loop
+//Compilacion  y interpretacion, JS ituliza una combinacion JIT
+//Abstract sintax three(AST) es lo q hay detras de una escritura de codigo, es lo que le envia al motor
