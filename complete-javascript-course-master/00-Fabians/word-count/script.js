729cc1220ddf2a0deb3e35795cc4b23b195eb24e
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-11-03 23:38:03 -0400

    changes

---
+++ complete-javascript-course-master/00-Fabians/word-count/script.js
@@ -0,0 +1,14 @@
+// Use the acumulator pattern
+//Letther counts in a word
+const palabra = "palabra";
+
+const palabraContador = {};
+for (let i = 0; i < palabra.length; i++) {
+  const letra = palabra[i];
+  if (!palabraContador[letra]) {
+    palabraContador[letra] = 1;
+  } else {
+    palabraContador[letra]++;
+  }
+}
+console.log(palabraContador);
