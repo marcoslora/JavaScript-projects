9106359e6d7b65cae625250cd01e92660634194f
marcoslora <marcoslora95@gmail.com>
2022-11-16 13:02:43 -0400

    Alterna

---
+++ complete-javascript-course-master/0-Alterna projects/01-fundamentos/cover. celsium a farent/script.js
@@ -0,0 +1,18 @@
+'use strict';
+const celsius = 40;
+const celsiusToFahrenheit = celsius * (9 / 5) + 32;
+console.log(`La temperatura en Fahrenheit es ${celsiusToFahrenheit}`);
+
+const fahrenheitToCelsius = ((104 - 32) * 5) / 9;
+console.log(`La temperatura en celsius es ${fahrenheitToCelsius}`);
+const km = 20;
+const kmToMillas = km / 1.6;
+console.log(kmToMillas);
+// &&
+const leapYear = 2020;
+if ((leapYear % 4 == 0 && leapYear % 100 != 0) || leapYear % 400 == 0) {
+  console.log(`${leapYear} es bisiesto`);
+} else {
+  console.log(`${leapYear} no es bisiesto`);
+}
+console.log(leapYear % 100);
