e3ffc8f8a166445bbc991d37ad09576dd4b6c5ba
marcoslora <marcoslora95@gmail.com>
2022-11-18 13:17:41 -0400

    for/while

--- complete-javascript-course-master/0-Alterna projects/01-fundamentos/18-nov/script.js
+++ complete-javascript-course-master/0-Alterna projects/01-fundamentos/18-nov/script.js
@@ -11,4 +11,49 @@ if (numberAbsoluto < 0) {
 //Condicion ? que hacer : else
 const tenaryOpe = numberAbsoluto < 0 ? -numberAbsoluto : numberAbsoluto;
 console.log(tenaryOpe);
+
+//parseFloat y parseInt
+//Truthy objectos, numeros != 0, strings
+//Falsy 0, string vacio, NaN
+// type Season = 'back friday' | 'christmas';
+
 //Switch
+const precio = 4000;
+const temporada = 'christmas';
+switch (temporada) {
+  case 'black friday':
+    console.log(precio * 0.8);
+    break;
+  case 'christmas':
+    console.log(precio * 0.9);
+    break;
+  default:
+    console.log(precio);
+}
+
+// while
+let numero = -3901;
+//Cambia a valor absoluto
+numero = numero < 0 ? -numero : numero;
+let digito;
+let residuo;
+let level = numero.toString().length - 1;
+//Math.pow elevar a la potencia
+//Math.floor devuelve un numero entero quitandole los decimales
+//Math.trunc los redondea a un numero entero
+while (level >= 0) {
+  digito = Math.floor(numero / Math.pow(10, level));
+  numero = numero % Math.pow(10, level);
+  console.log(digito);
+  level = level - 1;
+}
+//For
+for (let i = 1; i <= 5; i += 1) {
+  for (let j = 1; j <= i; j += 1) {
+    console.log(i);
+  }
+}
+const numeroTabla = 10;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numeroTabla} x ${i} = ${numeroTabla * i}`);
+}
