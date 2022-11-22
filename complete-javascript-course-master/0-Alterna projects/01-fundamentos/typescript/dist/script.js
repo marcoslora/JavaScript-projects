51e37f8d8a837bc4e9141461bb8a949055c16a04
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-11-22 16:06:49 -0400

    Changes

---
+++ complete-javascript-course-master/0-Alterna projects/01-fundamentos/typescript/dist/script.js
@@ -0,0 +1,5 @@
+"use strict";
+//0-Alterna\ projects/01-fundamentos/typescript/
+const nombre = 'Marcos';
+console.log(nombre);
+console.log('hola');
---
+++ complete-javascript-course-master/0-Alterna projects/01-fundamentos/typescript/src/script.ts
@@ -0,0 +1,4 @@
+//0-Alterna\ projects/01-fundamentos/typescript/
+const nombre: string = 'Marcos';
+console.log(nombre);
+console.log('hola');
