9250776b4565f30c114b86528a754c1d77fda861
marcoslora <marcoslora95@gmail.com>
2022-11-23 22:45:26 -0400

    Changes

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -32,3 +32,21 @@ console.log(restaurant.categories[1]);
 //Destructuring arrays
 const [first, , second] = restaurant.categories;
 console.log(first, second);
+
+const noFormatText = 'alTErNa AcaDEMy eS lo MEJOR'
+  .toLocaleLowerCase()
+  .split(' ');
+
+function capitalize(str) {
+  //charAt atrapa el index de cada letra del string
+  //slice captura de ese en adelante
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+const copyItems = [];
+noFormatText.forEach(str => {
+  copyItems.push(capitalize(str));
+});
+console.log(copyItems.join(' '));
+
+// const capitalizeString = noFormatText.split(' ').map(capitalize).join(' ');
+// console.log(capitalizeString);
