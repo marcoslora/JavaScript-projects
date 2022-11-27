27488dbbea85f8b6907afb58e67b7f587aadf906
marcoslora <95894058+marcoslora@users.noreply.github.com>
2022-11-26 21:41:45 -0400

    Changes

--- complete-javascript-course-master/00-Alterna-projects/01-fundamentos/calc-prest/dist/script.js
+++ complete-javascript-course-master/00-Alterna-projects/01-fundamentos/calc-prest/dist/script.js
@@ -1,22 +1,50 @@
 'use strict';
 //Calculadora laboral
 //00-Alterna-projects/01-fundamentos/calc-prest/
-const sueldo = 10000;
-const tiempo = 366;
+const sueldo = Math.round(10000 / 23.83);
+const tiempo = 2555;
 const preAviso = true;
+const vacaciones = true;
+const initDate = new Date('12/23/2021').getTime();
+const endDate = new Date(2022, 11, 23).getTime();
+const labourDays = (endDate - initDate) / 1000 / 60 / 60 / 24;
+console.log(labourDays);
 function calcPreAviso(ingr, time) {
   let preAvisoMonto = 0;
   if (preAviso && time >= 90 && time <= 180) {
-    preAvisoMonto = (ingr / 30) * 7;
+    preAvisoMonto = ingr * 7;
   }
   if (preAviso && time > 180 && time <= 365) {
-    preAvisoMonto = (ingr / 30) * 14;
+    preAvisoMonto = ingr * 14;
   }
   if (preAviso && time >= 366) {
-    preAvisoMonto = (ingr / 30) * 28;
+    preAvisoMonto = ingr * 28;
   }
-  preAvisoMonto = parseFloat(preAvisoMonto.toFixed(2));
-  return preAvisoMonto;
+  return parseFloat(preAvisoMonto.toFixed(2));
 }
+function calcCesantia(ingr, time) {
+  let amountCesantia = 0;
+  if (time >= 90 && time <= 180) {
+    amountCesantia = ingr * 6;
+  }
+  if (time > 180 && time <= 365) {
+    amountCesantia = ingr * 13;
+  }
+  if (time > 365 && time < 1825) {
+    amountCesantia = ingr * (21 * (time / 365));
+  }
+  if (time >= 1825) {
+    amountCesantia = ingr * (23 * (time / 365));
+  }
+  return parseFloat(amountCesantia.toFixed(2));
+}
+function calcVacaciones(ingr, vacations, time) {
+  let amountVacaions = 0;
+  if (vacations && time >= 365) {
+    amountVacaions = ingr * 14;
+  }
+  return parseFloat(amountVacaions.toFixed(2));
+}
+function calcNavidad(ingr, time) {}
 console.log(calcPreAviso(sueldo, tiempo));
-function calcCesantia(ingr, time) {}
+console.log(calcCesantia(sueldo, tiempo));
--- complete-javascript-course-master/00-Alterna-projects/01-fundamentos/calc-prest/src/script.ts
+++ complete-javascript-course-master/00-Alterna-projects/01-fundamentos/calc-prest/src/script.ts
@@ -1,36 +1,53 @@
 //Calculadora laboral
 //00-Alterna-projects/01-fundamentos/calc-prest/
-const sueldo: number = 10000;
-const tiempo: number = 190;
+const sueldo: number = Math.round(10000 / 23.83);
+const tiempo: number = 2555;
 const preAviso: boolean = true;
 const vacaciones: boolean = true;
+const initDate = new Date('12/23/2021').getTime();
+const endDate = new Date('12/23/2022').getTime();
+const labourDays = (endDate - initDate) / 1000 / 60 / 60 / 24;
+console.log(typeof labourDays);
 
-function calcPrestaciones(ingr: number, time: number): number {
+function calcPreAviso(ingr: number, time: number): number {
   let preAvisoMonto: number = 0;
 
   if (preAviso && time >= 90 && time <= 180) {
-    preAvisoMonto = (ingr / 30) * 7;
+    preAvisoMonto = ingr * 7;
   }
   if (preAviso && time > 180 && time <= 365) {
-    preAvisoMonto = (ingr / 30) * 14;
+    preAvisoMonto = ingr * 14;
   }
   if (preAviso && time >= 366) {
-    preAvisoMonto = (ingr / 30) * 28;
+    preAvisoMonto = ingr * 28;
   }
   return parseFloat(preAvisoMonto.toFixed(2));
 }
 
-function calcCesantia(ingr: number, time: number) {
-  if (time) {
+function calcCesantia(ingr: number, time: number): number {
+  let amountCesantia: number = 0;
+  if (time >= 90 && time <= 180) {
+    amountCesantia = ingr * 6;
   }
+  if (time > 180 && time <= 365) {
+    amountCesantia = ingr * 13;
+  }
+  if (time > 365 && time < 1825) {
+    amountCesantia = ingr * (21 * (time / 365));
+  }
+  if (time >= 1825) {
+    amountCesantia = ingr * (23 * (time / 365));
+  }
+  return parseFloat(amountCesantia.toFixed(2));
 }
 function calcVacaciones(ingr: number, vacations: boolean, time: number) {
   let amountVacaions: number = 0;
   if (vacations && time >= 365) {
-    amountVacaions = (ingr / 30) * 14;
+    amountVacaions = ingr * 14;
   }
   return parseFloat(amountVacaions.toFixed(2));
 }
 
 function calcNavidad(ingr: number, time: number) {}
-console.log(calcPrestaciones(sueldo, tiempo));
+console.log(calcPreAviso(sueldo, tiempo));
+console.log(calcCesantia(sueldo, tiempo));
