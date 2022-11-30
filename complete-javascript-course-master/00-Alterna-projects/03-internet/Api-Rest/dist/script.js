2f65f0bbd5b896390a51244f3d93084ad1ca13b1
marcoslora <marcoslora95@gmail.com>
2022-11-30 13:09:56 -0400

    nodejs express

--- complete-javascript-course-master/00-Alterna-projects/03-internet/Api-Rest/dist/script.js
+++ complete-javascript-course-master/00-Alterna-projects/03-internet/Api-Rest/dist/script.js
@@ -1,1 +1,9 @@
 'use strict';
+//00-Alterna-projects/03-internet/Api-Rest
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hellos World');
+});
+app.listen(5000);
