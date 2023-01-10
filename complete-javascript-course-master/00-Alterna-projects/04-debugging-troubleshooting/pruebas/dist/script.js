e0fc9f989ac1da0580a7456523424173e4043e32
marcoslora <95894058+marcoslora@users.noreply.github.com>
2023-01-10 10:55:37 -0400

    changes

--- complete-javascript-course-master/00-Alterna-projects/04-debugging-troubleshooting/pruebas/dist/script.js
+++ complete-javascript-course-master/00-Alterna-projects/04-debugging-troubleshooting/pruebas/dist/script.js
@@ -1,17 +1,19 @@
+"use strict";
+////00-Alterna-projects/04-debugging-troubleshooting/pruebas
 function factorial(num) {
-  // Si num = 0 OR num = 1, el factorial regresa 1
-  if (num === 0 || num === 1) return 1;
-
-  // Empezamos el bucle FOR con i = 4
-  //Decremento i después de cada iteración
-  for (let i = num - 1; i > 0; i--) {
-    //Guardamos el valor de num en cada iteración
-    num = num * i; // o num *= i;
-  }
-  return num; //120
+    if (num === 0 || num === 1)
+        return 1;
+    for (let i = num - 1; i > 0; i--) {
+        num = num * i;
+    }
+    return num;
 }
 console.log(factorial(5));
-
-for (let i = 5; i <= 10; i++) {
-  console.log(i);
+function factorial1(num) {
+    let result = 0;
+    for (let i = num - 1; i > 0; i--) {
+        result = num *= i;
+    }
+    return result;
 }
+console.log(factorial1(5));
---
+++ complete-javascript-course-master/00-Alterna-projects/04-debugging-troubleshooting/pruebas/src/script.ts
@@ -0,0 +1,19 @@
+////00-Alterna-projects/04-debugging-troubleshooting/pruebas
+function factorial(num: number): number {
+  if (num === 0 || num === 1) return 1;
+
+  for (let i = num - 1; i > 0; i--) {
+    num = num * i;
+  }
+  return num;
+}
+console.log(factorial(5));
+
+function factorial1(num: number): number {
+  let result = 0;
+  for (let i = num - 1; i > 0; i--) {
+    result = num *= i;
+  }
+  return result;
+}
+console.log(factorial1(5));
