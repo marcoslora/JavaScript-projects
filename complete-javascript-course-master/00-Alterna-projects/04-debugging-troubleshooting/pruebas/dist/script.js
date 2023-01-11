9bda2bf03befdd2b3b38fee0145a9d324b7ac35b
marcoslora <marcoslora95@gmail.com>
2023-01-11 10:23:07 -0400

    factorial

--- complete-javascript-course-master/00-Alterna-projects/04-debugging-troubleshooting/pruebas/dist/script.js
+++ complete-javascript-course-master/00-Alterna-projects/04-debugging-troubleshooting/pruebas/dist/script.js
@@ -1,19 +1,18 @@
-"use strict";
+'use strict';
 ////00-Alterna-projects/04-debugging-troubleshooting/pruebas
 function factorial(num) {
-    if (num === 0 || num === 1)
-        return 1;
-    for (let i = num - 1; i > 0; i--) {
-        num = num * i;
-    }
-    return num;
+  if (num === 0 || num === 1) return 1;
+  for (let i = num - 1; i > 0; i--) {
+    num = num * i;
+  }
+  return num;
 }
 console.log(factorial(5));
 function factorial1(num) {
-    let result = 0;
-    for (let i = num - 1; i > 0; i--) {
-        result = num *= i;
-    }
-    return result;
+  let result = 0;
+  for (let i = num - 1; i > 0; i--) {
+    result = num *= i;
+  }
+  return result;
 }
-console.log(factorial1(5));
+console.log(factorial1(10));
