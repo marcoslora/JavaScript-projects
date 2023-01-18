72fdad0d8cfb37e171dc3e00e7af7969e45e6c1a
marcoslora <95894058+marcoslora@users.noreply.github.com>
2023-01-18 13:57:50 -0400

    changes

---
+++ complete-javascript-course-master/00-Alterna-projects/04-debugging-troubleshooting/ex/reverseWords/dist/script.js
@@ -0,0 +1,11 @@
+function reverseWords(s) {
+  s.split(' ')
+    .map(word => word.split('').reverse().join(''))
+    .join(' ');
+}
+
+let input = "Let's reverse the words on this sentence";
+console.log(input + ' => ' + reverseWords(input));
+
+input = 'Up Down';
+console.log(input + ' => ' + reverseWords(input));
