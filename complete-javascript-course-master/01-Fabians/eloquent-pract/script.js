506ffd5b7979ad8f80161dfe85487470a332dece
marcoslora <marcoslora95@gmail.com>
2023-05-09 15:57:25 -0400

    changes

--- complete-javascript-course-master/01-Fabians/eloquent-pract/script.js
+++ complete-javascript-course-master/01-Fabians/eloquent-pract/script.js
@@ -30,3 +30,99 @@ for (let y = 0; y < size; y++) {
   board += '\n';
 }
 console.log(board);
+const humus = function (factor) {
+  const ingrediente = function (cantidad, unidad, nombre) {
+    let cantidadIngrediente = cantidad * factor;
+    if (cantidadIngrediente > 1) {
+      unidad += 's';
+    }
+    console.log(`${cantidadIngrediente} ${unidad} ${nombre}`);
+  };
+  ingrediente(1, 'lata', 'garbanzos');
+  ingrediente(0.25, 'taza', 'tahini');
+  ingrediente(0.25, 'taza', 'jugo de limón');
+  ingrediente(1, 'clavo', 'ajo');
+  ingrediente(2, 'cucharada', 'aceite de oliva');
+  ingrediente(0.5, 'cucharadita', 'comino');
+};
+humus(2);
+function envolverValor(n) {
+  let local = n;
+  return (x = 0) => x + local;
+}
+let envolver1 = envolverValor(1);
+let envolver2 = envolverValor(2);
+console.log(envolver1(10));
+// → 1
+console.log(envolver2());
+// → 2
+
+function encontrarSolucion(objetivo) {
+  function encontrar(actual, historial) {
+    console.log(actual > objetivo, actual, objetivo);
+    if (actual == objetivo) {
+      return historial;
+    } else if (actual > objetivo) {
+      return null;
+    } else {
+      return (
+        encontrar(actual + 5, `(${historial} + 5 )`) ||
+        encontrar(actual * 3, `(${historial} * 3)`)
+      );
+    }
+  }
+  return encontrar(1, '1');
+}
+console.log(encontrarSolucion(13));
+
+function encontrarSolucion1(objetivo) {
+  function encontrar(actual, historial) {
+    if (actual == objetivo) {
+      return [historial];
+    } else if (actual > objetivo) {
+      return [];
+    } else {
+      const soluciones = [
+        ...encontrar(actual + 5, `(${historial} + 5)`),
+        ...encontrar(actual * 3, `(${historial} * 3)`),
+      ];
+      return soluciones;
+    }
+  }
+
+  const soluciones = encontrar(1, '1');
+  let solucionMasCorta = null;
+
+  for (const solucion of soluciones) {
+    if (
+      solucionMasCorta === null ||
+      solucion.length < solucionMasCorta.length
+    ) {
+      solucionMasCorta = solucion;
+    }
+  }
+
+  return solucionMasCorta;
+}
+
+console.log(encontrarSolucion1(101));
+
+function imprimirInventarioGranja(vacas, pollos) {
+  let stringVaca = String(vacas);
+  while (stringVaca.length < 3) {
+    stringVaca = '0' + stringVaca;
+  }
+  console.log(`${stringVaca} Vacas`);
+  let stringPollos = String(pollos);
+  while (stringPollos.length < 3) {
+    stringPollos = '0' + stringPollos;
+  }
+  console.log(`${stringPollos} Pollos`);
+}
+imprimirInventarioGranja(7, 11);
+
+function esPar(x) {
+  if (x % 2 == 0) return true;
+  else return false;
+}
+console.log(esPar(50));
