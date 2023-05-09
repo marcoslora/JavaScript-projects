164c076890e0e94f6f332e3dfa4546459571410f
marcoslora <marcoslora95@gmail.com>
2023-05-09 18:30:34 -0400

    change

---
+++ Fabian projects/login-authentificacion-firebase/src/app/firebase.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBcgltQZkT4hRfW4Eh93ebWowdD2ZjBU-M",
+authDomain: "placas-registro-bebe.firebaseapp.com",
+projectId: "placas-registro-bebe",
+storageBucket: "placas-registro-bebe.appspot.com",
+messagingSenderId: "847119301834",
+appId: "1:847119301834:web:cd653fd8441ae5a5010833"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
